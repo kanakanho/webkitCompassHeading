263d322f02a12bce551b4bb95859b37ad783446e
kanakanho <kahoshibakichi@gmail.com>
2026-02-04 23:20:11 +0900

    fix

--- src/main.ts
+++
@@ -1,7 +0,0 @@
-window.addEventListener("deviceorientation", (event: DeviceOrientationEvent) => {
-    if (event.webkitCompassHeading !== undefined) {
-        console.log(`Compass Heading: ${event.webkitCompassHeading}`);
-    } else {
-        console.log("webkitCompassHeading is not supported on this device.");
-    }
-});
